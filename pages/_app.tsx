d61bb98aa26f85010428ca303e55fb871bfa42ce
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-11-15 23:39:15 +0300

    _app.tsx setup and Supabase setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,35 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+// add Supabase and nextui providers
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { useState } from "react";
+import { NextUIProvider } from "@nextui-org/react";
+import { Box } from "../components/Box";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient);
+  // TODO: create a supabase account
+  // TODO: make Navbar
+  // TODO: make Box component for body content
+
+  return (
+    <SessionContextProvider supabaseClient={supabaseClient}>
+      <NextUIProvider>
+        {/* insert navbar here */}
+        <Box
+          css={{
+            px: "$12",
+            py: "$15",
+            mt: "$12",
+            "@xsMax": { px: "$10" },
+            maxWidth: "800px",
+            margin: "0 auto",
+          }}
+        >
+          <Component {...pageProps} />
+        </Box>
+      </NextUIProvider>
+    </SessionContextProvider>
+  );
 }
