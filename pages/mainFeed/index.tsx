c1ca90ae467167631a2e8e548716562ae4e04186
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-11-17 12:27:58 +0300

    create mainFeed page

---
+++ pages/mainFeed/index.tsx
@@ -0,0 +1,74 @@
+import type { NextPage } from "next";
+import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useRouter } from "next/router";
+import { Text } from "@nextui-org/react";
+import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { GetServerSidePropsContext } from "next";
+import { useState, useEffect } from "react";
+
+const MainFeed: NextPage = () => {
+  const supabaseClient = useSupabaseClient();
+  const user = useUser();
+  const router = useRouter();
+
+  const [articles, setArticles] = useState<string[]>([]);
+
+  useEffect(() => {
+    getArticles();
+  }, []);
+
+  const getArticles = async () => {
+    try {
+      const { data, error } = await supabaseClient
+        .from("articles")
+        .select("*")
+        .limit(10); //get top 10 rows from supabase client
+      console.log(data);
+      if (data != null) {
+        setArticles(data);
+      }
+    } catch (err: any) {
+      alert(err.message);
+    }
+  };
+
+  return (
+    <>
+      <Text h2>Main Feed</Text>
+      <Text size="$lg" css={{ my: "$8" }}>
+        Check out articles from users here
+      </Text>
+      {/* Article Card */}
+    </>
+  );
+};
+
+export default MainFeed;
+
+// HANDLING IF USER LOGGED IN
+// ==========================
+
+// export const getServerSideProps = withPageAuth({ redirectTo: "/login" }); //deprecated
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  // Create authenticated Supabase Client
+  const supabase = createServerSupabaseClient(ctx);
+  // Check if we have a session
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (!session)
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
+
+  return {
+    props: {
+      initialSession: session,
+      user: session.user,
+    },
+  };
+};
